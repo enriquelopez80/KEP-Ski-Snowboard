940555770dc003895c94af3691e219ba61e3c037
Kamon <Kamon.kirker@gmail.com>
2018-08-28 17:46:00 -0700

    handlebars href routes

--- public/js/index.js
+++ public/js/index.js
@@ -34,74 +34,75 @@ $("#right-womens").on("click", function(event) {
 //*************/
 
   //MENS//
-  $("#mensJackets").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("mens", "jackets", logResponse)
-  })
-  $("#mensFleece").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("mens", "fleece", logResponse)
-  })
-  $("#mensVests").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("mens", "vests", logResponse)
-  })
-  $("#mensPants").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("mens", "pants", logResponse)
-  })
-  //WOMENS//
-  $("#womensJackets").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("womens", "jackets", logResponse)
-  })
-  $("#womensFleece").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("womens", "fleece", logResponse)
-  })
-  $("#womensVests").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("womens", "vests", logResponse)
-  })
-  $("#womensPants").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("womens", "pants", logResponse)
-  })
-  //EQUIPMENT - SKI//
-  $("#skis").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("ski", "skis", logResponse)
-  })
-  $("#skiBindings").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("ski", "bindings", logResponse)
-  })
-  $("#skiBoots").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("ski", "boots", logResponse)
-  })
-  $("#skis").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("ski", "skis", logResponse)
-  })
-  //EQUIPMENT - SNOW//
-  $("#snowboards").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("snowboard", "boards", logResponse)
-  })
-  $("#snowBindings").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("snowboard", "bindings", logResponse)
-  })
-  $("#snowBoots").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("snowboard", "boots", logResponse)
-  })
-  //SAFETY - HELMETS//
-  $(".helmets").on("click", function (event) {
-    event.preventDefault();
-    ajaxGet("equipment", "helmets", logResponse)
-  })
+  // $("#mensJackets").on("click", function (event) {
+  //   // event.preventDefault();
+  //   // ajaxGet("mens", "jackets", logResponse)
+  // })
+
+  // $("#mensFleece").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("mens", "fleece", logResponse)
+  // })
+  // $("#mensVests").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("mens", "vests", logResponse)
+  // })
+  // $("#mensPants").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("mens", "pants", logResponse)
+  // })
+  // //WOMENS//
+  // $("#womensJackets").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("womens", "jackets", logResponse)
+  // })
+  // $("#womensFleece").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("womens", "fleece", logResponse)
+  // })
+  // $("#womensVests").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("womens", "vests", logResponse)
+  // })
+  // $("#womensPants").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("womens", "pants", logResponse)
+  // })
+  // //EQUIPMENT - SKI//
+  // $("#skis").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("ski", "skis", logResponse)
+  // })
+  // $("#skiBindings").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("ski", "bindings", logResponse)
+  // })
+  // $("#skiBoots").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("ski", "boots", logResponse)
+  // })
+  // $("#skis").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("ski", "skis", logResponse)
+  // })
+  // //EQUIPMENT - SNOW//
+  // $("#snowboards").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("snowboard", "boards", logResponse)
+  // })
+  // $("#snowBindings").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("snowboard", "bindings", logResponse)
+  // })
+  // $("#snowBoots").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("snowboard", "boots", logResponse)
+  // })
+  // //SAFETY - HELMETS//
+  // $(".helmets").on("click", function (event) {
+  //   event.preventDefault();
+  //   ajaxGet("equipment", "helmets", logResponse)
+  // })
   
 //************UTILS:**************//
 //********************************//
