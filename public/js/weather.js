158d9c8002b02c5b2c2f881c31792648b55b7ca4
Kamon <Kamon.kirker@gmail.com>
2018-09-04 19:44:41 -0700

    weather ready for styling

--- public/js/weather.js
+++ public/js/weather.js
@@ -40,6 +40,11 @@ $(document).ready(function () {
             console.log('click');
             getWeather("aspen");
           });
+
+          $('#mammoth-active').on('click', function() {
+            console.log('click');
+            getWeather("Mammoth");
+          });
     
     
     
@@ -48,44 +53,56 @@ $(document).ready(function () {
     //UTILS//
     //redners weather to html
     let renderWeather = (weather) => {
+
+        let monthDay1 = weather.list[3].dt_txt.split("-");
+        let monthDayFixed1 = monthDay1[2].split(" ");
+        let monthDay2 = weather.list[11].dt_txt.split("-");
+        let monthDayFixed2 = monthDay2[2].split(" ");
+        let monthDay3 = weather.list[19].dt_txt.split("-");
+        let monthDayFixed3 = monthDay3[2].split(" ");
+        let monthDay4 = weather.list[27].dt_txt.split("-");
+        let monthDayFixed4 = monthDay4[2].split(" ");
+        let monthDay5 = weather.list[35].dt_txt.split("-");
+        let monthDayFixed5 = monthDay5[2].split(" ");
+      
         $('#main-box').html(`
         <h1>${weather.city.name}</h1>
         `)
     
         $('#day1-box').html(`
         <h1>${weather.list[3].weather[0].description}</h1>
-        <h1>${weather.list[3].dt_txt}</h1>
+        <h1>${monthDay1[1]}/${monthDayFixed1[0]}</h1>
         <img style="width:20%;height:20%;border:0;" src="http://openweathermap.org/img/w/${weather.list[3].weather[0].icon}.png">
         <h1>${weather.list[3].main.temp}&#176</h1>
         `)
     
         $('#day2-box').html(`
         <div>
-        <h2>${weather.list[10].weather[0].description}</h2>
-        <h1>${weather.list[10].dt_txt}</h1>
-        <a><img style="width:20%;height:20%;border:0;" src="http://openweathermap.org/img/w/${weather.list[10].weather[0].icon}.png"></a>
-        <h1>${weather.list[10].main.temp}&#176</h1>
+        <h2>${weather.list[11].weather[0].description}</h2>
+        <h1>${monthDay2[1]}/${monthDayFixed2[0]}</h1>
+        <a><img style="width:20%;height:20%;border:0;" src="http://openweathermap.org/img/w/${weather.list[11].weather[0].icon}.png"></a>
+        <h1>${weather.list[11].main.temp}&#176</h1>
         `)
     
         $('#day3-box').html(`
-        <h1>${weather.list[18].weather[0].description}</h1>
-        <h1>${weather.list[18].dt_txt}</h1>
-        <img style="width:20%;height:20%;border:0;" src="http://openweathermap.org/img/w/${weather.list[18].weather[0].icon}.png">
-        <h1>${weather.list[18].main.temp}&#176</h1>
+        <h1>${weather.list[19].weather[0].description}</h1>
+        <h1>${monthDay3[1]}/${monthDayFixed3[0]}</h1>
+        <img style="width:20%;height:20%;border:0;" src="http://openweathermap.org/img/w/${weather.list[19].weather[0].icon}.png">
+        <h1>${weather.list[19].main.temp}&#176</h1>
         `)
     
         $('#day4-box').html(`
-        <h1>${weather.list[26].weather[0].description}</h1>
-        <h1>${weather.list[26].dt_txt}</h1>
-        <img style="width:20%;height:20%;border:0;" src="http://openweathermap.org/img/w/${weather.list[26].weather[0].icon}.png">
-        <h1>${weather.list[26].main.temp}&#176</h1>
+        <h1>${weather.list[27].weather[0].description}</h1>
+        <h1>${monthDay4[1]}/${monthDayFixed4[0]}</h1>
+        <img style="width:20%;height:20%;border:0;" src="http://openweathermap.org/img/w/${weather.list[27].weather[0].icon}.png">
+        <h1>${weather.list[27].main.temp}&#176</h1>
         `)
     
         $('#day5-box').html(`
-        <h1>${weather.list[34].weather[0].description}</h1>
-        <h1>${weather.list[34].dt_txt}</h1>
-        <img style="width:20%;height:20%;border:0;" src="http://openweathermap.org/img/w/${weather.list[34].weather[0].icon}.png">
-        <h1>${weather.list[34].main.temp}&#176</h1>
+        <h1>${weather.list[35].weather[0].description}</h1>
+        <h1>${monthDay5[1]}/${monthDayFixed5[0]}</h1>
+        <img style="width:20%;height:20%;border:0;" src="http://openweathermap.org/img/w/${weather.list[35].weather[0].icon}.png">
+        <h1>${weather.list[35].main.temp}&#176</h1>
         `)
     
     }
